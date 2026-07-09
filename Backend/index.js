9916e1c0cfce11be45561b2ac2f79ba84cfb1bbd
Harish singh <singhharish7317@gmail.com>
2026-07-10 01:40:12 +0530

    update the url

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "react-project-8mi05ydid-team-jarvish.vercel.app",
+    ],
+  })
+);;
 app.use(express.json());
 
 app.get("/", (req, res) => {
